445b7e4974683ba7551ffa8012ab5f7c74ef485c
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2023-06-01 09:58:41 +0300

    here we go

--- snakeClass.py
+++ snakeClass.py
@@ -16,6 +16,7 @@ import datetime
 import distutils.util
 DEVICE = 'cpu' # 'cuda' if torch.cuda.is_available() else 'cpu'
 
+
 #################################
 #   Define parameters manually  #
 #################################
